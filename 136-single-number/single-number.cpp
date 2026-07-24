f9e638ff82642fd649aae669c446c408f8085474
Meet Sahu <meetsahu012@gmail.com>
2026-07-24 22:36:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (59.21%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        int ans = 0;
+        for(int num : nums){
+            ans ^= num;
         }
-
-        for(auto it : mp){
-            if(it.second == 1) return it.first;
-        }
-        return -1;
+        return ans;
     }
 };
