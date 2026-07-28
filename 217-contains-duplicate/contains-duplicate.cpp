eb185e974a04ca56b1a211ea4c39163085eca7c4
Meet Sahu <meetsahu012@gmail.com>
2026-07-28 19:30:47 +0530

    Time: 76 ms (24.72%) | Memory: 111.5 MB (14.96%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int> mp;
 
         for(int i=0;i<nums.size();i++){
-            if(mp.count(nums[i])) return true;
+            if(mp.count(nums[i]) != NULL) return true;
 
             mp[nums[i]] = i;
         }
