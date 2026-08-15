c5e2e7bc6ada70a25fd3038a8aa394666c94fc52
Meet Sahu <meetsahu012@gmail.com>
2026-08-15 23:46:41 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (93.38%) - LeetSync

--- 4384-elevator-requests-i/elevator-requests-i.cpp
+++ 4384-elevator-requests-i/elevator-requests-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int elevatorRequests(int n, vector<int>& requests) {
-        int count = requests[0]-0;
+        int count = requests[0];
 
         for(int i=1;i<requests.size();i++){
             count += abs(requests[i-1] - requests[i]);
