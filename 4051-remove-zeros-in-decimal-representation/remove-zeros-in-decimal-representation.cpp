df35bed1c1f86c29fb311784b8aa6d09ce8e92ae
Meet Sahu <meetsahu012@gmail.com>
2026-09-22 00:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (84.72%) - LeetSync

---
+++ 4051-remove-zeros-in-decimal-representation/remove-zeros-in-decimal-representation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long removeZeros(long long n) {
+        long long ans = 0;
+        long long place = 1;
+
+        while(n > 0){
+            int digit = n%10;
+            n /= 10;
+
+            if(digit != 0){
+                ans += digit * place;
+                place *= 10;
+            }
+        }
+        return ans;
+    }
+};
